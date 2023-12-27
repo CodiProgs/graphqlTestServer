b99353bd1b99e48d899ff35d4f73167a98493833
CodiProgs <samuil.codiWork@mail.ru>
2023-12-27 22:37:44 +0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import * as graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.js';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: true,
     credentials: true,
     allowedHeaders: [
       'Accept',
